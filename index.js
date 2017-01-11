5b5208444d42fc6fa31867f277f0af17606ffd11
Devan Wong <devan.wong@yahoo.com>
2017-01-10 20:55:15 -0800

    new photos in portfolio and weather app added

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-$("aboutMeB").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".aboutMe").offset().top},
-        'slow');
-});
+	$(document).ready(function(){
+		$(window).scroll(function(){
+			if ($(this).scrollTop() > 100) {
+				$('.scrollToTop').fadeIn();
+			} else {
+				$('.scrollToTop').fadeOut();
+			}
+		});
 
+//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+
+	$("a").click(function(){
+		console.log(this.hash)
+		if(this.hash){
+			$(window).scrollTo($(this.hash), {duration:800})
+		}
+	}
+	)});
